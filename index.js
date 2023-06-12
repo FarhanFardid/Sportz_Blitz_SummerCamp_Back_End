029928ad963b343e8c5989479a6bf8958d1fedea
FarhanFardid <farhanfardid009@gmail.com>
2023-06-12 14:53:31 +0600

    jwt installed and verify jwt added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+// JWT
+const jwt = require('jsonwebtoken')
 
 // dotenv configuration
 require("dotenv").config();
+
+
 
 const cors = require("cors");
 
@@ -12,11 +16,30 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+// Verify JWT
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(401).send({ error: true, message: "Unauthorized Access" });
+  }
+  const token = authorization.split(" ")[1];
+  // console.log(token);
+  jwt.verify(token, process.env.Access_Token_Secret, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 app.get("/", (req, res) => {
   res.send("Sports Blitz camp is going on....");
 });
 
-// sports_camp
+
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.joz6qi9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -49,6 +72,7 @@ app.get('/users', async(req,res)=>{
   res.send(result)
 })
 
+
   app.post('/users',async(req,res)=>{
     const user = req.body;
     const query = {email: user.email}
@@ -58,6 +82,16 @@ app.get('/users', async(req,res)=>{
     }
     const result = await usersCollection.insertOne(user);
     res.send(result);
+  })
+
+  // -----------------------------
+  //      Jwt Api
+  //---------------------------
+
+  app.post('/jwt', async(req,res)=>{
+    const user = req.body;
+    const token = jwt.sign(user,process.env.Access_Token_Secret,{expiresIn:'10h'}) 
+    res.send({token})
   })
 
   // -----------------------------
@@ -84,6 +118,24 @@ app.get('/users', async(req,res)=>{
     const result = await cartCollection.insertOne(selectedClass);
     res.send(result);
     
+  })
+
+  app.get('/cart', async(req,res)=>{
+    const email = req.query.email;
+    if(!email){
+      res.send([])
+    }
+    const query = {email : email}
+    const result = await cartCollection.find(query).toArray();
+    res.send(result);
+
+  })
+
+  app.delete('/cart/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)}
+    const result = await cartCollection.deleteOne(query);
+    res.send(result)
   })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
